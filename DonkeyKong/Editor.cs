93b3a6afc7dc0d84e3b36eff7d75b54ebaef08cd
Aric Crosson Bouwers <aricbouwers@outlook.com>
2021-09-24 10:50:38 -0600

    Adding forms with buttons - play game and start editor

---
+++ DonkeyKong/Editor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonkeyKong
+{
+    class Editor
+    {
+        string name;
+
+        Editor(string name) 
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ DonkeyKong/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonkeyKong
+{
+    public partial class Form1 : Form
+    {
+        NewLevel levelDialog; //this is declaring that it will exist
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            levelDialog = new NewLevel(); // this is instantiating it, ie. creating it
+            levelDialog.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
